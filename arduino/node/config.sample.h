4be0566d6a278ece23476838fb64c85dfafc8b8d
Dimitris Karakasilis <jimmykarily@gmail.com>
2017-11-08 21:22:04 +0200

    Update espduino code

---
+++ arduino/node/config.sample.h
@@ -0,0 +1,8 @@
+// This file is an example of what a credentials.h file should look like.
+// Replace the values with those matching your installation and copy the file
+// to "credentials.h" for a successful compilation
+#define MQTT_SERVER "THE_IP/HOST_OF_YOUR_MQTT_SERVER"
+#define NODE "THE_NAME_OF_THE_NODE"  // Also used as a username for mqtt
+#define MQTT_PASSWORD "PASSWORD"     // The mqtt password that matches the NODE
+#define WIFI_SSID "THE_SSID_OF_YOUR_WIFI"
+#define WIFI_PASSWORD "THE_PASSPHRASE_OF_YOUR_WIFI"
--- arduino/node/node.ino
+++ arduino/node/node.ino
@@ -1,77 +1,188 @@
-// An Arduino running this program will try to register itself to a smart pie
-// server. After successfully registering, it will start accepting commands
-// from the server.
-#include "ESP8266WiFi.h"
+/*
 
-// Holds Wifi credentials SSID and application token.
-#include "credentials.h"
+Smart home with arduino
 
-const char* host = "www.in.gr";
+This is the arduino code that connects to the mosquitto server and subscribes
+to the channels the control the state of the output pins.
 
-// the setup function runs once when you press reset or power the board
+The valid actions for pins are:
+
+- high
+  The specified pin will go to HIGH state no matter what the current state is
+- low
+  The specified pin will go to LOW state no matter what the current state is
+- toggle
+  When the pin is in HIGH state it will become LOW
+  When the pin is in LOW state is will become HIGH
+
+Examples:
+
+- Digital pin 4 must go to HIGH state. The message "high" should be received
+  on the <NODE>/pin/4
+- Digital pin 3 must go to LOW state. The message "low" should be received
+  on the <NODE>/pin/3
+- Digital pin 5 must toggle state. The message "toggle" should be received
+  on the <NODE>/pin/5
+
+The mosquitto server requires the clients to authenticate with username and
+password. The user name of each node is the value stored in NODE. This variable
+is set in config.h file. The WiFi credentials and SSID are also stored in
+the same file.
+
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "config.h"
+
+// This is a heartbeat. How often should this node advertise its enabled ports?
+#define ADVERTISE_INTERVAL_MILLIS 10000
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long last_heartbeat = 0;
+char heartbeat_text[50];
+
 void setup() {
+  // pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+
+  int number_of_enabled_pins = sizeof(ENABLED_PINS) / sizeof(int);
+  for (int i=0; i<=number_of_enabled_pins; i++) {
+    pinMode(ENABLED_PINS[i], OUTPUT);
+  }
+
   Serial.begin(115200);
+  setup_wifi();
+  client.setServer(MQTT_SERVER, 1883);
+  client.setCallback(callback);
+}
+
+void setup_wifi() {
+  delay(10); // TODO: Do we need this?
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  // while wifi not connected yet, print '.'
-  // then after it connected, get out of the loop
+
   while (WiFi.status() != WL_CONNECTED) {
-     delay(500);
-     Serial.print(".");
+    delay(500);
+    Serial.print(".");
   }
-  //print a new line, then print WiFi connected and the IP address
+
   Serial.println("");
   Serial.println("WiFi connected");
-  // Print the IP address
+  Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
-int value = 0;
+int extract_pin_from_topic(char* topic) {
+  int i = 0;
+  int len = strlen(topic);
+  int slashes=0;
+  char pin_string[3];
+  int pin_i=0;
 
-// the loop function runs over and over again forever
-void loop() {
-  delay(20000);
-
-  Serial.println("WiFi connected");
-  Serial.println(WiFi.localIP());
-
-  ++value;
-
-  Serial.print("connecting to ");
-  Serial.println(host);
-
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-  const int httpPort = 80;
-  if (!client.connect(host, httpPort)) {
-    Serial.println("connection failed");
-    return;
-  }
-
-  // We now create a URI for the request
-  String url = "/";
-  Serial.print("Requesting URL: ");
-  Serial.println(url);
-
-  // This will send the request to the server
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +
-               "Connection: close\r\n\r\n");
-  unsigned long timeout = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout > 5000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      return;
+  // Skip two '/' and the rest is the pin number
+  for (int i=0;i<=len;i++) {
+    if(slashes < 2) { // more to skip
+      if(topic[i] == '/') { slashes++; }
+    } else { // keep everything after the second '/'
+      pin_string[pin_i] = topic[i];
+      pin_i++;
     }
   }
 
-  // Read all the lines of the reply from server and print them to Serial
-  while(client.available()){
-    String line = client.readStringUntil('\r');
-    Serial.print(line);
+  return atoi(pin_string);
+}
+
+String action_from_payload(byte* payload, unsigned int length) {
+  char action[length];
+  memcpy(action, payload, length);
+  action[length] = '\0';
+
+  return action;
+}
+
+String enabled_ports_string() {
+  char text[21];
+  char pin_num[3];
+  int number_of_enabled_pins = sizeof(ENABLED_PINS) / sizeof(int);
+
+  strcpy(text, "");
+  for(int i=0; i<number_of_enabled_pins; i++) {
+    strcat(text, itoa(ENABLED_PINS[i], pin_num, 10));
+    // Skip the comma for the last element
+    if(i != number_of_enabled_pins - 1){
+      strcat(text, ",");
+    }
   }
 
+  return text;
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String action = action_from_payload(payload, length);
+  int pin = extract_pin_from_topic(topic);
+
   Serial.println();
-  Serial.println("closing connection");
+  Serial.print("Applying ");
+  Serial.print(action);
+  Serial.print(" to pin ");
+  Serial.println(pin);
+
+  if(strcmp(action.c_str(),"high") == 0) {
+    digitalWrite(pin, HIGH);
+  }
+  else if(strcmp(action.c_str(), "low") == 0) {
+    digitalWrite(pin, LOW);
+  }
+  else if(strcmp(action.c_str(), "toggle") == 0) {
+    digitalWrite(pin, !digitalRead(pin));
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client", NODE, MQTT_PASSWORD)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      // client.publish("outTopic", "hello world");
+
+      // Subscribe to one channel for each enabled pin
+      int number_of_enabled_pins = sizeof(ENABLED_PINS) / sizeof(int);
+      for (int i=0; i<number_of_enabled_pins; i++) {
+        char topic[256];
+        snprintf(topic, sizeof(topic), "%s%s%d", NODE, "/pin/", ENABLED_PINS[i]);
+        Serial.println(topic);
+        client.subscribe(topic);
+      }
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - last_heartbeat > ADVERTISE_INTERVAL_MILLIS) {
+    last_heartbeat = now;
+    sprintf (heartbeat_text, "%s:%s", NODE, enabled_ports_string().c_str());
+    Serial.print("Sending heartbeat: ");
+    Serial.println(heartbeat_text);
+    client.publish("heartbeats", heartbeat_text);
+  }
 }
